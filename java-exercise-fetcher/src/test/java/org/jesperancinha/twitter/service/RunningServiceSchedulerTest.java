db930400840e3f0ce1c3caa2c00b9d0396ec1692
Joao Esperancinha <jofisaes@gmail.com>
2020-01-17 14:55:45 +0100

    Running Service tests

---
+++ java-exercise-fetcher/src/test/java/org/jesperancinha/twitter/service/RunningServiceSchedulerTest.java
@@ -0,0 +1,32 @@
+package org.jesperancinha.twitter.service;
+
+import org.jesperancinha.twitter.client.TwitterClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@SpringBootTest
+@ActiveProfiles("scheduler")
+public class RunningServiceSchedulerTest {
+
+    @Autowired
+    private RunningService runningService;
+
+    @MockBean
+    private TwitterClient twitterClient;
+
+    @Test
+    public void testStartProcess_whenScheduler_thenSchedularInstance() throws InterruptedException {
+        assertThat(runningService).isInstanceOf(SchedulerServiceImpl.class);
+
+        verify(twitterClient, only()).startFetchProcess();
+        verifyNoMoreInteractions(twitterClient);
+    }
+}
