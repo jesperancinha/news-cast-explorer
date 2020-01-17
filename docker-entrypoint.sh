332bcfe71745bfbe6ec086ade1050f7c0a968498
Joao Esperancinha <jofisaes@gmail.com>
2020-01-17 15:31:10 +0100

    Update jar entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@ nginx
 cd /usr/local/bin
 cat /etc/nginx/nginx.conf
 python3 log-docker-server.py &
-java -jar -Dspring.profiles.active=scheduler --org.jesperancinha.twitter.consumerKey=<consumerKey> --org.jesperancinha.twitter.consumerSecret=<consumerSecret> --org.jesperancinha.twitter.token=<token> --org.jesperancinha.twitter.tokenSecret=<tokenSecret>
+java -jar -Dspring.profiles.active=scheduler /usr/local/bin/java-exercise-fetcher-1.0.0-SNAPSHOT.jar --org.jesperancinha.twitter.consumerKey=<consumerKey> --org.jesperancinha.twitter.consumerSecret=<consumerSecret> --org.jesperancinha.twitter.token=<token> --org.jesperancinha.twitter.tokenSecret=<tokenSecret>
