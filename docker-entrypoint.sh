0dee5a069f3d7e3df1a986d2de0a0abaa2f51f56
Joao Esperancinha <jofisaes@gmail.com>
2020-04-10 08:47:05 +0200

    Start of fast track HBC

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,11 @@ nginx
 cd /usr/local/bin
 cat /etc/nginx/nginx.conf
 python3 log-docker-server.py &
-java -jar -Dspring.profiles.active=scheduler /usr/local/bin/java-exercise-fetcher-1.0.0-SNAPSHOT.jar --org.jesperancinha.twitter.consumerKey=Jyrj0HRFWwmE6mbxuJTSwjcJ7 --org.jesperancinha.twitter.consumerSecret=jyYtPCauiNawrBNoK56nAMUVK6LHIlN2wiItnhgd5LdKZgK2fW --org.jesperancinha.twitter.token=1200086940604928005-8CVE1ZMl198Vqyxqcuol6qSsItQogZ --org.jesperancinha.twitter.tokenSecret=KxEbWs4oqhc8RvoBm7HYJSTQXrBqbooXewgv9mSoJDNFQ
+java -jar -Dspring.profiles.active=scheduler \
+  /usr/local/bin/java-exercise-fetcher-1.0.0-SNAPSHOT.jar \
+  --org.jesperancinha.twitter.searchTerm \
+  --org.jesperancinha.twitter.consumerKey=Jyrj0HRFWwmE6mbxuJTSwjcJ7 \
+  --org.jesperancinha.twitter.consumerSecret=jyYtPCauiNawrBNoK56nAMUVK6LHIlN2wiItnhgd5LdKZgK2fW \
+  --org.jesperancinha.twitter.token=1200086940604928005-8CVE1ZMl198Vqyxqcuol6qSsItQogZ \
+  --org.jesperancinha.twitter.tokenSecret=KxEbWs4oqhc8RvoBm7HYJSTQXrBqbooXewgv9mSoJDNFQ \
+  --org.jesperancinha.twitter.searchTerm=ritaora
