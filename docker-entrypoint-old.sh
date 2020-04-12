d6eaa6163f9291c954e707b934225198ce5adea8
Joao Esperancinha <jofisaes@gmail.com>
2020-04-12 22:36:57 +0200

    Code Cleanup

--- docker-entrypoint-old.sh
+++ docker-entrypoint-old.sh
@@ -5,7 +5,6 @@ cat /etc/nginx/nginx.conf
 python3 log-docker-server.py &
 java -jar -Dspring.profiles.active=scheduler \
   /usr/local/bin/twitter-fetcher-1.0.0-SNAPSHOT.jar \
-  --org.jesperancinha.twitter.searchTerm \
   --org.jesperancinha.twitter.consumerKey=AAAAAAAAAAAAAAAAAAAAAAAAA \
   --org.jesperancinha.twitter.consumerSecret=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA \
   --org.jesperancinha.twitter.token=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA \
