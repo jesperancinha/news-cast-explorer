f9641407918fe8c159e38e4dc4bd95b98067553d
Joao Esperancinha <jofisaes@gmail.com>
2022-04-03 19:13:30 +0200

    🌿 Merge all script

---
+++ merge-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git fetch -p
+git branch | grep -v master |  xargs -I {} git checkout {} \
+  git pull \
+  git merge --strategy-option theirs \
+  git checkout master \
+  git merge {}
