ee9393cc7d986ebade839530fb59586e94493a8c
Joao Esperancinha <jofisaes@gmail.com>
2022-04-03 19:55:46 +0200

    🌿 Merge all script

--- merge-all.sh
+++ merge-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 git fetch -p
 for branch in $(git branch | grep -v master)
 do
@@ -7,6 +8,12 @@ do
     git merge --strategy-option theirs
     git checkout master
     git merge "$branch"
+done
+
+for branch in $(git branch | grep -v master)
+do
+    git branch -d "$branch"
+    git push origin :refs/branches/"$branch"
 done
 
 git checkout master
