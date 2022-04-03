83e06ee734d64f14ec67a26b80407be5b310443e
Joao Esperancinha <jofisaes@gmail.com>
2022-04-03 19:22:06 +0200

    🌿 Merge all script

--- merge-all.sh
+++ merge-all.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 git fetch -p
-git branch | grep -v master |  xargs -I {} git checkout {} |
-  git pull |
-  git merge --strategy-option theirs |
-  git checkout master |
-  git merge {}
+all_branches=$(git branch | grep -v master)
+
+for branch in all_branches
+do
+    git checkout $branch
+    git pull
+    git merge --strategy-option theirs
+    git checkout master
+    git merge $branch
+done
+
+git checkout master
