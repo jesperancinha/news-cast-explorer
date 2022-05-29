e1b8af53717eec5fa1f9cc910985271415cd0e8d
Joao Esperancinha <jofisaes@gmail.com>
2022-05-29 16:12:38 +0200

    Test

---
+++ news-cast-explorer-fe/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  moduleNameMapper: {
+    '^.+.(svg|css|scss)$': 'jest-transform-stub',
+  },
+  "testEnvironment": "jsdom",
+  "setupFilesAfterEnv": [
+    "<rootDir>/setuptests.ts"
+  ],
+  preset: 'jest-preset-angular',
+  "testMatch": [
+    "**/?(*.)+(spec|test).[jt]s?(x)"
+  ]
+}
---
+++ news-cast-explorer-fe/src/setuptests.ts
@@ -0,0 +1,12 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import 'jest-preset-angular/setup-jest'
+import '@testing-library/jest-dom';
+import '@babel/preset-env';
+import '@babel/core';
+import 'zone.js';
+import 'tslib';
+import 'ts-node';
+import 'typescript';
