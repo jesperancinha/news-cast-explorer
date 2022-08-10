0bf6a925df7c7b2d1c0c0ed0cd2108e043578828
Joao Esperancinha <jofisaes@gmail.com>
2022-08-10 23:10:18 +0200

    PostgreSQL connection details

--- news-cast-explorer-fetcher/src/main/java/org/jesperancinha/newscast/config/ExecutorServiceWrapper.java
+++ news-cast-explorer-fetcher/src/main/java/org/jesperancinha/newscast/config/ExecutorServiceWrapper.java
@@ -1,5 +1,6 @@
 package org.jesperancinha.newscast.config;
 
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -15,6 +16,9 @@ public class ExecutorServiceWrapper {
     }
 
     private void init() {
+        if(Objects.nonNull(this.executorService)){
+            this.executorService.shutdown();
+        }
         this.executorService = Executors.newFixedThreadPool(3);
     }
 
