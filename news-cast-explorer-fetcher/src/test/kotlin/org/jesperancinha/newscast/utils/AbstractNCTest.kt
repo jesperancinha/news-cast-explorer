053db8e5fb84fd0d1e656adcf0093a3cda7624a9
Joao Esperancinha <jofisaes@gmail.com>
2022-08-10 22:29:55 +0200

    PostgreSQL connection details

--- news-cast-explorer-fetcher/src/test/kotlin/org/jesperancinha/newscast/utils/AbstractNCTest.kt
+++ news-cast-explorer-fetcher/src/test/kotlin/org/jesperancinha/newscast/utils/AbstractNCTest.kt
@@ -1,9 +1,5 @@
 package org.jesperancinha.newscast.utils
 
-import com.github.dockerjava.api.model.ExposedPort
-import com.github.dockerjava.api.model.HostConfig
-import com.github.dockerjava.api.model.PortBinding
-import com.github.dockerjava.api.model.Ports
 import io.kotest.mpp.log
 import io.mockk.impl.log.Logger
 import org.junit.platform.commons.logging.LoggerFactory
@@ -20,25 +16,13 @@ open class AbstractNCTest {
     companion object {
         private val logger = LoggerFactory.getLogger(AbstractNCTest::class.java)
 
-        private const val POSTGRESQL_PORT = 5432
-
         @Container
         @JvmField
         val postgreSQLContainer: TestPostgresSQLContainer = TestPostgresSQLContainer("postgres:15beta2")
             .withUsername("postgres")
             .withPassword("admin")
             .withDatabaseName("eventuate")
-            .withExposedPorts(POSTGRESQL_PORT)
-            .withCreateContainerCmdModifier { cmd ->
-                cmd.withHostConfig(
-                    HostConfig().withPortBindings(
-                        PortBinding(
-                            Ports.Binding.bindPort(POSTGRESQL_PORT),
-                            ExposedPort(POSTGRESQL_PORT)
-                        )
-                    )
-                )
-            }
+
 
         init {
             postgreSQLContainer.start()
@@ -48,9 +32,9 @@ open class AbstractNCTest {
         @JvmStatic
         fun setProperties(registry: DynamicPropertyRegistry) {
             logger.info { "Connection details to PostgreSQL:" }
-            logger.info { "Bound port numbers: ${postgreSQLContainer.boundPortNumbers}" }
+            logger.info { "Server Address: ${postgreSQLContainer.host}" }
             logger.info { "Exposed port numbers: ${postgreSQLContainer.exposedPorts}" }
-            registry.add("spring.datasource.url") { "jdbc:postgresql://localhost:${postgreSQLContainer.exposedPorts[0]}/eventuate" }
+            registry.add("spring.datasource.url") { "jdbc:postgresql://${postgreSQLContainer.host}:${postgreSQLContainer.exposedPorts[0]}/eventuate" }
             registry.add("spring.datasource.username") { "postgres" }
             registry.add("spring.datasource.password") { "admin" }
         }
