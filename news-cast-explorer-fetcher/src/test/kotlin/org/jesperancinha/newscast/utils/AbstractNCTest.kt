af88f49562b6e6a506ac9a11d62b9feb36987839
Joao Esperancinha <jofisaes@gmail.com>
2022-08-10 22:42:15 +0200

    PostgreSQL connection details

--- news-cast-explorer-fetcher/src/test/kotlin/org/jesperancinha/newscast/utils/AbstractNCTest.kt
+++ news-cast-explorer-fetcher/src/test/kotlin/org/jesperancinha/newscast/utils/AbstractNCTest.kt
@@ -1,7 +1,5 @@
 package org.jesperancinha.newscast.utils
 
-import io.kotest.mpp.log
-import io.mockk.impl.log.Logger
 import org.junit.platform.commons.logging.LoggerFactory
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
@@ -31,10 +29,14 @@ open class AbstractNCTest {
         @DynamicPropertySource
         @JvmStatic
         fun setProperties(registry: DynamicPropertyRegistry) {
+            val postgresHost = postgreSQLContainer.host
+            val postgresPort = postgreSQLContainer.getMappedPort(5432)
             logger.info { "Connection details to PostgreSQL:" }
-            logger.info { "Server Address: ${postgreSQLContainer.host}" }
-            logger.info { "Exposed port numbers: ${postgreSQLContainer.exposedPorts}" }
-            registry.add("spring.datasource.url") { "jdbc:postgresql://${postgreSQLContainer.host}:${postgreSQLContainer.exposedPorts[0]}/eventuate" }
+            logger.info { "Server Address: $postgresHost" }
+            logger.info { "Exposed port numbers: $postgresPort" }
+            registry.add("spring.datasource.url") {
+                "jdbc:postgresql://$postgresHost:$postgresPort/eventuate"
+            }
             registry.add("spring.datasource.username") { "postgres" }
             registry.add("spring.datasource.password") { "admin" }
         }
