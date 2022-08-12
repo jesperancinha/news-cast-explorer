e50480db8f05ec65e1d62b1b890bc6b24cbcf9b7
Joao Esperancinha <jofisaes@gmail.com>
2022-08-12 23:33:12 +0200

    Fetcher check

--- e2e/cypress/e2e/api/news.cast.app.spec.cy.js
+++ e2e/cypress/e2e/api/news.cast.app.spec.cy.js
@@ -4,6 +4,12 @@ describe('Opens application page', () => {
 
     it('shows swagger', () => {
         cy.visit(`http://${host}:${port}`);
+
+        cy.wait(30000);
+
+        cy.get('app-page-component > div > perfectscrollbar > table > tbody > tr').eq(0).click();
+        cy.get('app-author-component > div > perfectscrollbar > table > tbody > tr').eq(0).click();
+        cy.get('app-message-component > div > perfectscrollbar > table > tbody > tr').eq(0).should("exist");
     });
 
 })
--- e2e/cypress/e2e/swagger/fetcher/swagger.fetcher.spec.cy.js
+++ e2e/cypress/e2e/swagger/fetcher/swagger.fetcher.spec.cy.js
@@ -8,7 +8,7 @@ describe('Swagger Tests Fetcher', () => {
         cy.get('h2', {timeout: 10000}).contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
         cy.wait(1000);
 
-        cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:9000/api/fetcher')
+        cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:9000/api/newscast/fetcher')
     });
 
 })
