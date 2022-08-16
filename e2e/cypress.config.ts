c8a03cc2214e0217bb738b91488034d1eef74c6a
Joao Esperancinha <jofisaes@gmail.com>
2022-08-16 13:33:39 +0200

    Cypress Upgrade

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -5,7 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    includeShadowDom: true,
     supportFile: `${__dirname}/cypress/support/e2e.ts`,
     specPattern: `${__dirname}/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}`,
   },
