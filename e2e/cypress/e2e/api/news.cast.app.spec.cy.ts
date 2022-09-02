fc96d1d076b65be875a9f58f9cfea26d594bb578
Joao Esperancinha <jofisaes@gmail.com>
2022-09-02 19:18:09 +0200

    Updates Library

--- e2e/cypress/e2e/api/news.cast.app.spec.cy.ts
+++ e2e/cypress/e2e/api/news.cast.app.spec.cy.ts
@@ -1,6 +1,6 @@
 describe('Opens application page', () => {
     const host = Cypress.env('host') ? Cypress.env('host') : 'localhost';
-    const port = Cypress.env('port') ? Cypress.env('port') : '8083';
+    const port = Cypress.env('port') ? Cypress.env('port') : '4200';
 
     it('shows swagger', () => {
         cy.visit(`http://${host}:${port}`);
